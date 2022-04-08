7358123effc585809b21a549545a126dae09bd66
zabec <denis.imsirovic@gmail.com>
2022-04-08 18:35:12 +0200

    ex04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -4,8 +4,8 @@ function sumFibonacci(num) {
   var n2 = 1;
   var next = 0;
   var sum = 0;
-  for (var i = 1; n1 <= num; i++) {
-    (n1 % 2 == 1) ? sum += n1 : {};
+  for (var i = 1; i <= num; i++) {
+    (n2 % 2 == 1 && n2 <= num) ? sum += n2 : {};
     next = n1 + n2;
     n1 = n2;
     n2 = next;
